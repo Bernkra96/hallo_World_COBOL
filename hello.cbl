8db85e284bf7e41e5b38888ad70f83ad6e3fca2c
Bernkra96git config --global user.name Bernkra96 <bernkra@gmail.com>
2025-10-07 21:37:22 +0200

    first commit

---
+++ hello.cbl
@@ -0,0 +1,11 @@
+       identification division.
+       program-id. hello.
+       environment division.
+       data division.
+       procedure division.
+           display "Hello Wolrd"
+           stop run.
+            
+
+       
+        
